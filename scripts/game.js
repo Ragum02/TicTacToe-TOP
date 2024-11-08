0c20b0dee520fb49e8024dd478be41166e7fee91
Ragum02 <benjamin.souiket@gmail.com>
2024-11-08 16:00:34 +0100

    start the project aside my course with O'clock

---
+++ scripts/game.js
@@ -0,0 +1,35 @@
+const start = document.querySelector("#start");
+const restart = document.querySelector("#restart");
+
+const result = document.querySelector("#result");
+
+const cells = document.querySelector(".cell");
+
+let players = [];
+let currentPlayerIndex = 0;
+let gameFinish = false;
+
+let gameboard = ["","","","","","","","",""];
+
+const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+function startGame(){
+
+
+
+};
+
+function resetGame(){
+
+
+};
+
